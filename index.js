6fecd4eb541a2aab047974d64285a673fbb7d506
Gabriel Viox <gviox17@gmail.com>
2021-08-31 09:38:42 -0500

    commit tree 7f0c7df517958f682c96e73b54ff6191dcadf44e

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution in this file!
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+  function setBestCustomer() {
+    setBestCustomer();
+  }
+  function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+  }
+  function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Daniel";
+  }
